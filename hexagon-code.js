a2873b31ff550912fa59e5adc97198684ac545ff
Chocomint <86723911+ChocomintSSR@users.noreply.github.com>
2023-09-24 15:49:37 +0800

    complete hexagon version

--- hexagon-code.js
+++ hexagon-code.js
@@ -1,4 +1,76 @@
+function init(n, orientations) {
+	const colContainer = document.getElementById("col-container");
+	colContainer.innerHTML = "";
+	
+	const w = document.getElementsByClassName("container").item(0).clientWidth - 20;
+
+	for (let i = 0; i < n * 2 - 1; i++) {
+		const col = document.createElement("div");
+		col.className = "col";
+		
+		for (let j = ((i <= (n - 1)) ? 0 : (i - (n - 1))); j < ((i >= (n - 1)) ? (n * 2 - 1) : (i + Number(n))); j++) {
+			const button = document.createElement("button");
+			button.id = i + "," + j;
+			button.innerText = "↑";
+
+			button.style.width = w / (n * 2 - 1) + "px";
+			button.style.height = w / (n * 2 - 1) + "px";
+			button.style.borderRadius = w / (n * 4 - 2) + "px";
+			button.style.fontSize = w / (2.5 * (n * 2 - 1)) + "px";
+			button.style.rotate = "0deg";
+
+			button.addEventListener("click", () => {
+				turnNeighbor(n, button, orientations);
+			});
+
+			col.appendChild(button);
+		}
+		
+		colContainer.appendChild(col);
+	}
+}
+
+function turnNeighbor(n, button, orientations) {
+	const id = button.id;
+	const parts = id.split(",");
+	const x = parseInt(parts[0].trim());
+	const y = parseInt(parts[1].trim());
+	turnNeighborByPosition(n, x, y, orientations);
+}
+
+function turnNeighborByPosition(n, x, y, orientations) {
+	turnByPositionIfValid(n, x    , y    , orientations);
+	turnByPositionIfValid(n, x    , y - 1, orientations);
+	turnByPositionIfValid(n, x    , y + 1, orientations);
+	turnByPositionIfValid(n, x - 1, y    , orientations);
+	turnByPositionIfValid(n, x - 1, y - 1, orientations);
+	turnByPositionIfValid(n, x + 1, y    , orientations);
+	turnByPositionIfValid(n, x + 1, y + 1, orientations);
+}
+
+function turn(btn, orientations) {
+	var deg = parseInt(btn.style.rotate.replace("deg", ""));
+	btn.style.rotate = (deg + 360 / orientations) + "deg";
+}
+
+function turnByPositionIfValid(hex_n, x, y, orientations) {
+	if (x >= 0 && x < 2 * n - 1 && y >= 0
+		&& y >= ((x <= (n - 1)) ? 0 : (x - (n - 1)))
+		&& y < ((x >= (n - 1)) ? (n * 2 - 1) : (x + Number(n)))) {
+		turn(document.getElementById(x + "," + y), orientations);
+	}
+}
+
 function generate() {
 	n = document.getElementById("size").value
 	orientations = document.getElementById("orientation").value
+	init(n, orientations);
+
+	for (let i = 0; i < n * 2 - 1; i++) {
+		for (let j = ((i <= (n - 1)) ? 0 : (i - (n - 1))); j < ((i >= (n - 1)) ? (n * 2 - 1) : (i + Number(n))); j++) {
+			for (let t = 0; t < Math.floor(Math.random() * orientations); t++) {
+				turnNeighborByPosition(n, i, j, orientations);
+			}
+		}
+	}
 }
--- square-code.js
+++ square-code.js
@@ -9,22 +9,21 @@ function init(n, orientations) {
 		for (let j = 0; j < n; j++) {
 			const button = document.createElement("button");
 			button.id = i + "," + j;
+			button.innerText = "↑";
 
 			button.style.width = w / n + "px";
 			button.style.height = w / n + "px";
 			button.style.borderRadius = w / (2 * n) + "px";
 			button.style.fontSize = w / (2.5 * n) + "px";
+			button.style.rotate = "0deg";
 			
-			button.setAttribute("direction", 1);
 			button.addEventListener("click", () => {
 				turnNeighbor(n, button, orientations);
-				updateArrow(n, orientations);
 			});
 			row.appendChild(button);
 		}
 		rowContainer.appendChild(row);
 	}
-	updateArrow(n, orientations);
 }
 
 function turnNeighbor(n, button, orientations) {
@@ -32,7 +31,7 @@ function turnNeighbor(n, button, orientations) {
 	const parts = id.split(",");
 	const x = parseInt(parts[0].trim());
 	const y = parseInt(parts[1].trim());
-	turnNeighborByPosition(n, x, y, orientations)
+	turnNeighborByPosition(n, x, y, orientations);
 }
 
 function turnNeighborByPosition(n, x, y, orientations) {
@@ -48,12 +47,8 @@ function turnNeighborByPosition(n, x, y, orientations) {
 }
 
 function turn(btn, orientations) {
-	const currentNumber = parseInt(btn.getAttribute("direction"));
-	if (currentNumber == orientations) {
-		btn.setAttribute("direction", 1);
-	} else {
-		btn.setAttribute("direction", currentNumber + 1);
-	}
+	var deg = parseInt(btn.style.rotate.replace("deg", ""));
+	btn.style.rotate = (deg + 360 / orientations) + "deg";
 }
 
 function turnByPositionIfValid(grid_n, x, y, orientations) {
@@ -68,27 +63,9 @@ function generate() {
 	init(n, orientations);
 	for (let i = 0; i < n; i++) {
 		for (let j = 0; j < n; j++) {
-			for (let t = 0; t < Math.floor(Math.random() * (orientations - 1)); t++) {
+			for (let t = 0; t < Math.floor(Math.random() * orientations); t++) {
 				turnNeighborByPosition(n, i, j, orientations);
 			}
 		}
 	}
-	updateArrow(n, orientations);
 }
-
-function updateArrow(n, orientations) {
-	var orientations_symbols = [];
-	if (orientations == 4) {
-		orientations_symbols = ["↑", "→", "↓", "←"]
-	} else if (orientations == 8) {
-		orientations_symbols = ["↑", "↗", "→", "↘", "↓", "↙", "←", "↖"]
-	}
-
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < n; j++) {
-			const btn = document.getElementById(i + "," + j);
-			btn.textContent = orientations_symbols[btn.getAttribute("direction") - 1]
-		}
-	}
-}
-// ["⮝", "⮞", "⮟", "⮜"]
